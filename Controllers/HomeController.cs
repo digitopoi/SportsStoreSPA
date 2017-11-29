fb6376fd6c549cb71f73dc5fa9b5c37d2c1b92b5
digitopoi <hubo85@gmail.com>
2017-11-28 21:22:33 -0600

    Update home controller to check database connection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SportsStoreSPA.Models;
+using System.Linq;
 
 namespace SportsStoreSPA.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Message = "Sports Store App";
+            return View(_context.Products.First());
         }
     }
 }
