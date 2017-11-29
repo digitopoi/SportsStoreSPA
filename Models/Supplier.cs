21a9b6881a4f4fff1668bdfe5c69d4b3e609f9de
digitopoi <hubo85@gmail.com>
2017-11-28 20:41:08 -0600

    Add supplier model

---
+++ Models/Supplier.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SportsStoreSPA.Models
+{
+    public class Supplier
+    {
+        public long SupplierId { get; set; }
+
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
