f627fdd4ead7b669ae724261194c05d79d3709c7
digitopoi <hubo85@gmail.com>
2017-11-28 22:02:55 -0600

    add simple repository to get started

---
+++ ClientApp/app/models/repository.ts
@@ -0,0 +1,10 @@
+import { Product } from './product.model';
+
+export class Repository {
+
+    product: Product;
+
+    constructor() {
+        this.product = JSON.parse(document.getElementById('data').textContent);
+    }
+}
