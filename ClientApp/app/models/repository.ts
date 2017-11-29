a3026a1d920c518a810e33e6f36c789945f7b808
digitopoi <hubo85@gmail.com>
2017-11-28 23:24:28 -0600

    Get api data in the repository

--- ClientApp/app/models/repository.ts
+++ ClientApp/app/models/repository.ts
@@ -1,10 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
 import { Product } from './product.model';
 
+@Injectable()
 export class Repository {
 
     product: Product;
 
-    constructor() {
-        this.product = JSON.parse(document.getElementById('data').textContent);
+    constructor(private http: Http) {
+        this.getProduct(1);
+    }
+
+    getProduct(id: number) {
+        this.http.get('/api/products/' + id)
+            .subscribe(response => this.product = response.json());
     }
 }
+
